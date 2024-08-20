f637c9908d577a92f9b9306f52b6ee98c2d23368
Sahil <sahilverma22146@gmail.com>
2024-08-20 11:06:05 +0530

    Update NewTransations.js

--- expense-tracker-app/src/component/NewTransations.js
+++ expense-tracker-app/src/component/NewTransations.js
@@ -1,18 +1,39 @@
-function NewTransations() {
+// import { useRef } from "react";
+import History from "./History";
+function NewTransations({ expense ,listInput , amountInput, add}) {
+  
+
+  
+
   return (
     <>
       <div className="NewTransations">
         <h> Add NewTransations</h>
         <br></br>
         <label>Text</label>
-        <input placeholder="Type Of Expense " type="text"></input>
+        <input
+          ref={listInput}
+          placeholder="Type Of Expense "
+          type="text"
+        ></input>
 
         <label>Amount</label>
 
-        <input placeholder="Enter The Amount" type="number"></input>
+        <input ref={amountInput} placeholder="Enter The Amount" type="number"></input>
 
-        <button> Add Transation</button>
+        <button onClick={add}> Add Transation</button>
       </div>
+      <h> History</h>
+
+      <History></History>
+      {expense.map((items)=>{
+        return(<History
+           text={items.text}
+           expense={items.expense}
+           
+          ></History>)
+      }
+      )}
     </>
   );
 }
