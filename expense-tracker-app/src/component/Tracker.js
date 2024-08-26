dee6e1cf3f5c1151526dbf7673827ccd2074c30f
Sahil <sahilverma22146@gmail.com>
2024-08-26 22:16:13 +0530

    Update Tracker.js

--- expense-tracker-app/src/component/Tracker.js
+++ expense-tracker-app/src/component/Tracker.js
@@ -6,18 +6,20 @@ function Tracker() {
   const [expense, setExpense] = useState([]);
   const [price, setPrice] = useState(0);
   const [updatePrice, setUpdatePrice] = useState(price);
+  const [editingId, setEditingId] = useState(null);
 
-  const [updateTask, setUpdateTask] = useState(null);
+  const [toogle, setToogle] = useState(false);
+  // const [saveNew , setSaveNew]=useState(null);
 
   let listInput = useRef(null);
   let amountInput = useRef(null);
 
   function add() {
     const el = listInput.current.value;
-    console.log(el);
+    // console.log(el);
 
     const expenseInput = amountInput.current.value;
-    console.log(expenseInput);
+    // console.log(expenseInput);
 
     const newEl = {
       id: Date.now(),
@@ -33,27 +35,62 @@ function Tracker() {
 
     listInput.current.value = "";
     amountInput.current.value = "";
+
+    console.log("u r in add");
   }
 
   function deleteButton(id) {
     console.log(" the id is ", id);
     console.log("delete button was clicked");
+    const toBeDeleteed = expense.filter((items) => {
+      return items.id !== id;
+    });
 
-    setExpense(expense.filter((items) => items.id !== id));
+    setExpense(toBeDeleteed);
+    console.log(toBeDeleteed);
+
+    const newP = toBeDeleteed.map((vl) => {
+      return vl.expense;
+    });
+    setPrice(+newP);
+    console.log("price :", price);
+
+    setUpdatePrice(+price);
   }
 
-  function handleChange(){
-    console.log("chnge done")
+  function handleChange() {
+    console.log("chnge done");
   }
-
-  
 
   function editButton(id) {
-    console.log(" the edit button id is :", id);
-    console.log("editbutton ws clicked");
-    const getTask = expense.filter((el) => id === el.id);
-  
-    
+    const getTask = expense.find((el) => id === el.id);
+    console.log(getTask);
+    listInput.current.value = getTask.text;
+    amountInput.current.value = getTask.expense;
+    setEditingId(id);
+    console.log("the gettask is ", getTask);
+
+    setToogle(true);
+    console.log("u rin edit");
+  }
+
+  function update() {
+    const newVl = listInput.current.value;
+    const newExpense = amountInput.current.value;
+
+    console.log("u r in updte");
+
+    setExpense(
+      expense.map((elem) => {
+        if (elem.id === editingId) {
+          return { ...elem, text: newVl, expense: newExpense };
+        }
+      })
+    );
+
+    listInput.current.value = "";
+    amountInput.current.value = "";
+    setToogle(false);
   }
 
   return (
@@ -85,6 +122,8 @@ function Tracker() {
           deleteButton={deleteButton}
           editButton={editButton}
           handleChange={handleChange}
+          toogle={toogle}
+          update={update}
         ></NewTransations>
       </div>
     </>
