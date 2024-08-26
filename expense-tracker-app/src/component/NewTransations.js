30147195a3e22b158757fb763c9f5df5106f1a5a
Sahil <sahilverma22146@gmail.com>
2024-08-26 12:22:31 +0530

    Update NewTransations.js

--- expense-tracker-app/src/component/NewTransations.js
+++ expense-tracker-app/src/component/NewTransations.js
@@ -1,10 +1,14 @@
 // import { useRef } from "react";
 import History from "./History";
-function NewTransations({ deleteButton,expense, listInput , amountInput, add , editButton}) {
-  
-
-  
-
+function NewTransations({
+  deleteButton,
+  expense,
+  listInput,
+  amountInput,
+  add,
+  editButton,
+  handleChange
+}) {
   return (
     <>
       <div className="NewTransations">
@@ -15,31 +19,34 @@ function NewTransations({ deleteButton,expense, listInput , amountInput, add , e
           ref={listInput}
           placeholder="Type Of Expense "
           type="text"
+          onChange={handleChange}
         ></input>
 
         <label>Amount</label>
 
-        <input ref={amountInput} placeholder="Enter The Amount" type="number"></input>
+        <input
+          ref={amountInput}
+          placeholder="Enter The Amount"
+          type="number"
+          onChange={handleChange}
+        ></input>
 
         <button onClick={add}> Add Transation</button>
       </div>
       <h> History</h>
 
       <History></History>
-      {expense.map((items)=>{
-        return(
-        <History
-           id={items.id}
-           text={items.text}
-           expense={items.expense}
-           deleteButton={deleteButton}
-           editButton={editButton}
-
-          
-           
-          ></History>)
-      }
-      )}
+      {expense.map((items) => {
+        return (
+          <History
+            id={items.id}
+            text={items.text}
+            expense={items.expense}
+            deleteButton={deleteButton}
+            editButton={editButton}
+          ></History>
+        );
+      })}
     </>
   );
 }
